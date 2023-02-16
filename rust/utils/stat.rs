32f4c7bdf935dd33f41a207e379945ff2ba4f4ed
Eshban Suleman <eshban9492@gmail.com>
2023-02-17 01:30:20 +0500

    Added variance function to utils

--- rust/utils/stat.rs
+++ rust/utils/stat.rs
@@ -4,3 +4,13 @@ pub fn mean(values : &Vec<f32>) -> f32 {
     }
     return values.iter().sum::<f32>() / (values.len() as f32);
 }
+
+pub fn variance(values : &Vec<f32>) -> f32 {
+    if values.len() == 0 {
+        return 0f32;
+    }
+    let mean = mean(values);
+    return values.iter()
+            .map(|x| f32::powf(x - mean, 2 as f32))
+            .sum::<f32>() / values.len() as f32;
+}
