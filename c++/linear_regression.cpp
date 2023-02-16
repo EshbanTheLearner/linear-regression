89001b1201686a0775a587d65eea508cb6cb2988
Eshban Suleman <eshban9492@gmail.com>
2023-02-17 00:41:43 +0500

    Initialized variables

--- c++/linear_regression.cpp
+++ c++/linear_regression.cpp
@@ -6,3 +6,12 @@ bool custom_sort(double a, double b){
     return a1<b1;
 }
 
+int main(){
+    double x[] = {1, 2, 3, 4, 5};
+    double y[] = {1, 2, 3, 4, 5};
+    vector<double>error;
+    double devi;
+    double b0 = 0;
+    double b1 = 0;
+    double lr = 0.01;
+}
