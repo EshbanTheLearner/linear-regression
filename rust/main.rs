9aebf75e848df571ccf1874d5941f4c18fda5238
Eshban Suleman <eshban9492@gmail.com>
2023-02-18 18:08:10 +0500

    Added imports

---
+++ rust/main.rs
@@ -0,0 +1,11 @@
+extern crate ml_models;
+extern crate plotlib;
+
+use ml_models::regression::linear_regression;
+use ml_models::gradient_descent::gradient_descent;
+
+use plotlib::scatter::Scatter;
+use plotlib::scatter;
+use plotlib::style::{Marker, Point};
+use plotlib::view::View;
+use plotlib::page::Page;
