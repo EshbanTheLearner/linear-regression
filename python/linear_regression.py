6360e3f6e32413401fc06f8ab7887cdf547407bd
Eshban Suleman <eshban9492@gmail.com>
2023-02-19 22:48:40 +0500

    Added train-test split

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -5,3 +5,12 @@ y = np.sin(X) + 0.1 * np.power(X, 2) + 0.5 * np.random.randn(100, 1)
 X /= np.max(X)
 
 X = np.hstack((np.ones_like(X), X))
+
+order = np.random.permutation(len(X))
+split = 20
+
+test_x = X[order[:split]]
+test_y = y[order[:split]]
+
+train_x = X[order[split:]]
+train_y = y[order[split:]]
