2aa2e9369a102ed986a1e6025c96b6693d3a83cd
Eshban Suleman <eshban9492@gmail.com>
2023-02-19 22:47:20 +0500

    Added data

--- python/linear_regression.py
+++ python/linear_regression.py
@@ -1,2 +1,7 @@
 import numpy as np
 
+X = np.linspace(1.0, 10.0, 100)[:, np.newaxis]
+y = np.sin(X) + 0.1 * np.power(X, 2) + 0.5 * np.random.randn(100, 1)
+X /= np.max(X)
+
+X = np.hstack((np.ones_like(X), X))
